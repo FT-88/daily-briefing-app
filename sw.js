818f881960209ddee800275d79cdfc3184efa2ad
FT-88 <FT-88@users.noreply.github.com>
2026-07-08 22:00:07 +0800

    Add English text-to-speech playback for news and business tips

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-briefing-v1';
+const CACHE = 'daily-briefing-v2';
 const CORE = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
@@ -7,6 +7,11 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
